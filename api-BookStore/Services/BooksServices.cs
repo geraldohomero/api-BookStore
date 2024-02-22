f81947a79919bcaf35416c98ceb5842074af7cbf
Geraldo Homero <geraldohomero@protonmail.com>
2024-02-22 08:32:53 -0300

    Add CRUD

--- api-BookStore/Services/BooksServices.cs
+++ api-BookStore/Services/BooksServices.cs
@@ -13,9 +13,23 @@ namespace api_BookStore.Services
             var mongoClient = new MongoClient(
                 bookStoreDatabaseSettings.Value.ConnectionString);
             var mongoDatabase = mongoClient.GetDatabase(
-                bookStoreDatabaseSettings.Value.DatabaseName );
+                bookStoreDatabaseSettings.Value.DatabaseName);
             _booksCollection = mongoDatabase.GetCollection<Book>(
-                bookStoreDatabaseSettings.Value.BooksCollectionName);    
+                bookStoreDatabaseSettings.Value.BooksCollectionName);
         }
+        public async Task<List<Book>> GetAsync() =>
+            await _booksCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Book?> GetAsync(string id) =>
+            await _booksCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Book newBook) =>
+            await _booksCollection.InsertOneAsync(newBook);
+
+        public async Task UpdateAsync(string id, Book updatedBook) =>
+            await _booksCollection.ReplaceOneAsync(x => x.Id == id, updatedBook);
+
+        public async Task RemoveAsync(string id) =>
+            await _booksCollection.DeleteOneAsync(x => x.Id == id);
     }
 }
