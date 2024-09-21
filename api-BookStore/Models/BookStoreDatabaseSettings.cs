97a6769e69dfc571d76726baab9549145027738b
Geraldo Homero <geraldohomero@protonmail.com>
2024-09-21 13:23:42 -0300

    Update

--- api-BookStore/Models/BookStoreDatabaseSettings.cs
+++ api-BookStore/Models/BookStoreDatabaseSettings.cs
@@ -2,7 +2,6 @@
 {
     public class BookStoreDatabaseSettings
     {
-
         public string ConnectionString { get; set; } = null!;
         public string DatabaseName { get; set; } = null!;
         public string BooksCollectionName { get; set; } = null!;
